38deaf4db519790f40a4c1c08f2f09e14db974ab
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2022-09-22 19:48:31 +1200

    setup files

---
+++ lifehacks/objects/__main__.py
@@ -0,0 +1,24 @@
+'''	Copyright © 2022 mightbesimon.com
+	All rights reserved.
+
+	Material belonging to others may have been
+	used under Creative Commons Licence or with
+	explicit or implicit permission.
+'''
+
+if __name__ == '__main__':
+	print('''
+Copyright © 2022 mightbesimon.com
+All rights reserved.
+
+Material belonging to others may have been
+used under Creative Commons Licence or with
+explicit or implicit permission.
+
+################################################################
+#######                lifehacks.objects                 #######
+################################################################
+
+📦 lifehacks.objects
+└── Clist
+''')
